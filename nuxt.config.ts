4f214b08779545cad4f08ac07cc83f7007e8dc79
billbravo99 <billy.maulana@kpvendor.id>
2024-01-07 09:46:12 +0700

    chore: update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,46 @@
-import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
-  meta: {
-    title: 'Billy Maulana',
-    link: [
-      {
-        rel: 'icon', type: 'image/png', href: '/nuxt.png',
-      },
-    ],
-  },
-  nitro: {
-    preset: "vercel",
-  },
-  buildModules: [
+  modules: [
     '@vueuse/nuxt',
     '@unocss/nuxt',
     '@pinia/nuxt',
+    '@nuxtjs/color-mode',
   ],
-  vueuse: {
-    ssrHandlers: true,
+   experimental: {
+    // when using generate, payload js assets included in sw precache manifest
+    // but missing on offline, disabling extraction it until fixed
+    payloadExtraction: false,
+    renderJsonPayloads: true,
+    typedPages: true,
   },
-  unocss: {
-    uno: true,
-    attributify: true,
-    preflight: true,
-    icons: {
-      scale: 1.2,
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
+  colorMode: {
+    classSuffix: '',
+  },
+  nitro: {
+    preset: "vercel",
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
     },
-    shortcuts: [
-      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ],
   },
-  experimental: {
-    viteNode: true,
+
+  app: {
+    head: {
+      title: 'Billy Maulana',
+      viewport: 'width=device-width,initial-scale=1',
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: "Billy Maulana Portfolio" },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      ],
+    },
   },
 })
