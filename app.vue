ecc48f4d8f40461da340a88fd61dbc837d6f54d7
billymaulana <billymaulana1999@gmail.com>
2026-01-31 20:42:42 +0700

    feat: intro splash v1 with counter animation

--- app.vue
+++ app.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+const isLoading = ref(true)
+
+function handleSplashComplete() {
+  isLoading.value = false
+}
+
 useHead({
   htmlAttrs: { lang: 'en' },
+  bodyAttrs: { class: 'bg-black' },
 })
 </script>
 
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="app">
+    <IntroSplash v-if="isLoading" @complete="handleSplashComplete" />
+
+    <div v-show="!isLoading">
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
+  </div>
 </template>
